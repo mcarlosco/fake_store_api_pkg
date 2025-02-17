21d73adf9b3231029fa09d84e374b0371c47f169
Carlos Murillo <m.carlos.co@gmail.com>
2025-02-17 10:41:00 -0500

    Update parsers_util.dart

--- lib/src/utils/parsers_util.dart
+++ lib/src/utils/parsers_util.dart
@@ -7,9 +7,9 @@
 /// ```dart
 /// print(parseIntOrDefault('1')); // 1
 /// print(parseIntOrDefault('one')); // 0
-/// print(parseIntOrDefault('one', df: 1)); // 1
+/// print(parseIntOrDefault('one', 1)); // 1
 /// ```
-int parseIntOrDefault(String source, [int? radix, int df = 0]) =>
+int parseIntOrDefault(String source, [int df = 0, int? radix]) =>
     int.tryParse(source, radix: radix) ?? df;
 
 /// Parse [source] as a double literal and return its value.
@@ -21,7 +21,7 @@ int parseIntOrDefault(String source, [int? radix, int df = 0]) =>
 /// ```dart
 /// print(parseDoubleOrDefault('1.5')); // 1.5
 /// print(parseDoubleOrDefault('one.5')); // 0
-/// print(parseDoubleOrDefault('two.nine', df: 2.9)); // 2.9
+/// print(parseDoubleOrDefault('two.nine', 2.9)); // 2.9
 /// ```
 double parseDoubleOrDefault(String source, [double df = 0]) =>
     double.tryParse(source) ?? df;
